b0f298e1d9edcc1020a29f71db9fbd1162a7ac1b
dzzie <dzzie@yahoo.com>
2015-09-28 06:07:19 -0400

    wp8: added action and about panes, basic icons

--- PhoneApp/wp8.1/Humidor/PivotPage.xaml.cs
+++ PhoneApp/wp8.1/Humidor/PivotPage.xaml.cs
@@ -15,12 +15,28 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 using Windows.UI.ViewManagement;
-
-// The Pivot Application template is documented at http://go.microsoft.com/fwlink/?LinkID=391641
+using Windows.Web.Http;
+using Windows.Security.ExchangeActiveSyncProvisioning;
 
 namespace Humidor
 {
+    
+    public static class DeviceInfo
+    {
+        //http://igrali.com/2014/07/17/get-device-information-windows-phone-8-1-winrt/
+        private static EasClientDeviceInformation deviceInfo = new EasClientDeviceInformation();
+        
+        public static bool IsRunningOnEmulator
+        {
+            get
+            {
+                return (deviceInfo.SystemProductName == "Virtual");
+            }
+        }
+    }
+
     public sealed partial class PivotPage : Page
     {
 
@@ -28,7 +44,7 @@ namespace Humidor
         {
             this.InitializeComponent();
 
-            if (false)
+            if (DeviceInfo.IsRunningOnEmulator) 
             {
                 App.settings.uid = "6";
                 App.settings.apiKey = "test"; 
@@ -54,10 +70,40 @@ namespace Humidor
             if (this.pivot.SelectedIndex == 2) this.pivot.SelectedIndex = 0; //they just saved settings so lets jump to the display..
 
             //any why in the world doesnt Navigate accept a string? oghh yeah academics..
-            //yes yes i know should be https..this is not critical data, you can leave apikey out if you want only used for watered/smoked buttons
-            string url = "http://" + txtServer.Text + "/humidor/mobile.php?id=" + txtUserID.Text + "&apikey=" + txtApiKey.Text; // +"&page=" + this.pivot.SelectedIndex.ToString();            
+            string url = "http://" + txtServer.Text + "/humidor/mobile.php?id=" + txtUserID.Text;           
             wb.Navigate(new Uri(url));
             wb2.Navigate(new Uri(url + "&page=2"));
+        }
+
+        async private void doWebReq(string url)
+        {
+            
+            string ret = "";
+            try
+            {
+                var httpClient = new HttpClient();
+                var response = await httpClient.GetAsync(new Uri(url, UriKind.RelativeOrAbsolute));
+                ret = response.StatusCode.ToString();
+                response.EnsureSuccessStatusCode(); //If Response is not Http 200 then EnsureSuccessStatusCode will throw an exception
+                ret = await response.Content.ReadAsStringAsync();
+                showMessage("Server Response", ret, false);
+            }
+            catch (Exception ex) {
+                showMessage("Exception Caught", ret, false);
+            } 
+          
+        }
+
+        public async void showMessage(string title, string msg, bool fullScreen)
+        {
+            //MessageDialog message = new MessageDialog(msg);
+            //await message.ShowAsync();
+            ContentDialog contentDialog = new ContentDialog();
+            contentDialog.FullSizeDesired =fullScreen;
+            contentDialog.Title = title;
+            contentDialog.Content = msg;
+            contentDialog.PrimaryButtonText = "OK";
+            await contentDialog.ShowAsync();
         }
 
         //save button
@@ -67,6 +113,24 @@ namespace Humidor
             App.settings.apiKey = txtApiKey.Text;
             App.settings.server = txtServer.Text;
             ShowStats();
+        }
+
+        private void btnSmoked_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://" + txtServer.Text + "/humidor/logData.php?wasSmoked=1&clientid=" + txtUserID.Text + "&apikey=" + App.settings.apiKey;
+            doWebReq(url);
+        }
+
+        private void btnWatered_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://" + txtServer.Text + "/humidor/logData.php?wasWatered=1&clientid=" + txtUserID.Text + "&apikey=" + App.settings.apiKey;
+            doWebReq(url);
+        }
+
+        private void btnClearAlert_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://" + txtServer.Text + "/humidor/logData.phpclear_alert=1&clientid=" + txtUserID.Text + "&apikey=" + App.settings.apiKey;
+            doWebReq(url);
         }
 
         private void Current_SizeChanged(object sender, SizeChangedEventArgs e)
